20486b9e4848ec69db6052e5b31298c07e1d7e74
Vejune <vejune.p@gmail.com>
2023-03-14 17:02:19 +0200

    up

--- mapping_based_preproc.sh
+++ mapping_based_preproc.sh
@@ -3,7 +3,7 @@
 ## Reference genome preparation
 #Your script should check if index files exist and if it does not exist - create a reference genome index.
 
-hisat2-build ./ref/mm10.fa.gz  ./ref/mm10
+hisat2-build ./ref/mm10.fa  ./ref/mm10
 ## Data QC
 #Run FASTQC analysis on each of your FASTQC files.
 for i in inputs/*.gz
@@ -11,9 +11,16 @@ do fastqc $i
 done
 
 #Generate MULTIQC report for (1) results.
-MultiQC inputs/*.gz
+multiqc inputs/*fastqc.zip -o inputs/
 
-Run standard FASTQ trimming: you should remove adapters, trim low-quality bases as well as remove reads that are shorter than 20 bp.
+#Run standard FASTQ trimming: you should remove adapters, trim low-quality bases as well as remove reads that are shorter than 20 bp.
+for i in inputs/*1.fastq.gz
+do
+    base=$(basename $i _1.fastq.gz)
+    trim_galore -j 6 --paired inputs/${base}_1.fastq.gz inputs/${base}_2.fastq.gz -o outputs/
+done
+
+
 Rerun FASTQC on newly created/cleaned FASTQ files. 
 Create MultiQC plots for your raw and processed data. 
 ## Mapping, some QC and quantification
